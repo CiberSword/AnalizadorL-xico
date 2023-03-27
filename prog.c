6ad44954f5ee3948ac4ffe0c4bba55e90fe919cd
CiberSword <cs.danieljl@gmail.com>
2023-03-26 21:31:02 -0600

    First version
    Co-authored-by: Daniel Olivar <AxelOlivar@users.noreply.github.com>
    Co-authored-by: xTheGGx <xTheGGx@users.noreply.github.com>

---
+++ prog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main () {
+
+ent digito -> 0, numero;
+real xoxo -> 5.32;
+simb yop -> '@';
+largo -> 2677;
+octal -> 016;
+hexa -> 0x16;
+
+printf("Ejemplo del lenguaje nuevo");
+
+
+printf("%ld", largo);
+
+haz{
+printf("%d ",digito++);
+}mientras{
+    (digito<=9);
+}
+para(numero = 0; numero = 3 ; numero ++){
+	printf("%d", numero):
+}
+
+si (xoxo < 10.2){
+	printf("Es menor");
+}sino{
+	printf("No es menor");
+}
+
+regresa 0;
+}
